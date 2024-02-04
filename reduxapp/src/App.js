cf43b4758c529aee214ee2f3785bc704fd0cec61
8249596972 <subratmohanty2309@gmail.com>
2024-02-04 13:54:53 +0530

    redux-file

---
+++ reduxapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <h2> Welcome To ReactRedux App</h2>
+        </div>
+  )
+}
+
+export default App
